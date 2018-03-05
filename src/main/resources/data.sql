e23829f30414492968b3deae41f8ef28fe374275
OpenCircleEndy <endy.brosens@opencirclesolutions.com>
2018-03-05 22:14:29 +0100

    Setup test data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product values (random_uuid(), 'FREE', 'PUBLISHED');
+insert into product values (random_uuid(), 'PREMIUM', 'PUBLISHED');
+insert into product values (random_uuid(), 'FAMILY', 'TEST');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table product
+(
+   id UUID not null,
+   code varchar(255) not null,
+   status varchar(255) not null,
+);
